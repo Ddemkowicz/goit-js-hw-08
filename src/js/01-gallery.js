18bec06da1a3e34b310e309ea28d7207a0af13b3
Ddemkowicz <dakucp@gmail.com>
2023-03-03 06:51:49 +0100

    init

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,47 @@
 // Add imports above this line
-import { galleryItems } from './gallery-items';
+import { galleryItems } from './gallery-items.js';
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
+const basicLightbox = require('basiclightbox');
+import * as basicLightbox from 'basiclightbox';
 // Change code below this line
 
 console.log(galleryItems);
+
+const gallery = document.querySelector('.gallery');
+
+for (let i = 0; i < galleryItems.length; i++) {
+  const image = document.createElement('img');
+  image.classList = 'gallery__image';
+  image.src = galleryItems[i].preview;
+  image.alt = galleryItems[i].description;
+
+  const link = document.createElement('a');
+  link.classList = 'gallery__item';
+  link.href = galleryItems[i].original;
+
+  link.append(image);
+  gallery.append(link);
+
+  link.addEventListener('click', e => {
+    e.preventDefault();
+
+    // instance.show();
+
+    document.addEventListener('keydown', e => {
+      if (e.code === 'Escape') {
+        instance.close();
+      }
+    });
+  });
+}
+let lightbox = new SimpleLightbox('.gallery a', {
+  navText: ['←', '→'],
+  captions: true,
+  captionsSelector: 'img',
+  captionsType: 'attr',
+  captionsData: 'alt',
+  captionPosition: 'bottom',
+  captionDelay: 250,
+});
--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,15 @@
+// import Player from '@vimeo/player';
 
+const player = new Vimeo.Player('vimeo-player');
+
+player.on(
+  'timeupdate',
+  _.throttle(function (data) {
+    localStorage.setItem('videoplayer-current-time', data.seconds);
+  }, 1000)
+);
+
+const currentTime = localStorage.getItem('videoplayer-current-time');
+if (currentTime) {
+  player.setCurrentTime(currentTime);
+}
